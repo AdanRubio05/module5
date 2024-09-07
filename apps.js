288891e99cf8610f7a784c8b94b3706a99a6af68
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-07 11:19:38 -0500

    js, unexpected string error

--- apps.js
+++ apps.js
@@ -3,4 +3,7 @@ document.getElementById("intro").innerHTML = "In The Old Testament book, Genesis
 const cargo = {"Elephants", "Lions", "Dogs", "Cats", "everything that crawls or slithers."} ;
 
 let text = "" ;
-
+for (let i = 0; i < cargo.length; i++) {
+    text += cargo[i] + "<br>" ;
+}
+document.getElementById("animals").innerHTML = text ;
