fbb05d6d058eaaa9ddc91e5ce8ed1c5d4042ab1c
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-09-07 11:33:49 -0500

    grammar

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 document.getElementById("intro").innerHTML = "In The Old Testament book, Genesis, Noah was commanded by God to build an ark."
 
-document.getElementById("originz").innerHTML = "Two of every known animal species was brought aboard. These included:"
+document.getElementById("originz").innerHTML = "Two of every known animal species was to be brought aboard. These included:"
 
 const cargo = ["Elephants", "Lions", "Dogs", "Cats", "and everything that crawled or slithered."] ;
 
